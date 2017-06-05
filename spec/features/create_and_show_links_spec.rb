a07d45293447aa0b9cb75c4a3539bb4f1d79a9dd
Max Glassie <msglassie@gmail.com>
2017-06-05 09:18:57 -0600

    first commit of the day building out adding links test

--- spec/features/create_and_show_links_spec.rb
+++ spec/features/create_and_show_links_spec.rb
@@ -12,23 +12,13 @@ RSpec.describe "User can visit the index page", :js => :true do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
-  scenario "and see a list of existing links" do
+  scenario "and submit a link and title" do
     visit "/"
 
-    within('#link-form') do
-        expect(page).to have_content("Turing")
-        expect(page).to have_content("https://turing.io")
-        expect(page).to have_content("Read?: false")
-    end
-  end
-
-  xscenario "and submit a new link and title" do
-    visit "/"
-
-    within('#link-form') do
-        fill_in "link[url]", with: "we@thebest.com"
-        fill_in "link[title]", with: "password"
-        click_on "Save Link"
+    within('#link-create') do
+        fill_in "link-url", with: "https://www.turing.io/"
+        fill_in "link-title", with: "Turing"
+        click_on "Add Link"
     end
   end
 end
